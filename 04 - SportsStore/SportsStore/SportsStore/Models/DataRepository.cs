75909d36498278cf4a1afff1db6946ef760f8f9e
Wagner Xu <wagnerhsu@hotmail.com>
2019-05-12 15:55:35 +0800

    Upgrade babel to libman

--- 04 - SportsStore/SportsStore/SportsStore/Models/DataRepository.cs
+++ 04 - SportsStore/SportsStore/SportsStore/Models/DataRepository.cs
@@ -1,13 +1,15 @@
 ﻿using System.Collections.Generic;
 
-namespace SportsStore.Models {
-
-    public class DataRepository : IRepository {
+namespace SportsStore.Models
+{
+    public class DataRepository : IRepository
+    {
         private List<Product> data = new List<Product>();
 
         public IEnumerable<Product> Products => data;
 
-        public void AddProduct(Product product) {
+        public void AddProduct(Product product)
+        {
             this.data.Add(product);
         }
     }
