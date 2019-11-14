c3f7f7cd91621843c164a9f40d4a6940e17f4548
Wagner Xu <wagnerhsu@hotmail.com>
2019-11-14 13:32:27 +0800

    Add WpfAppTest01

---
+++ 16 - Working with Relationships 2/DataApp/WpfAppTest01/App.xaml.cs
@@ -0,0 +1,40 @@
+﻿using MeeHealth.NetStandard.NLogComponent;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.IO;
+using System.Windows;
+using DataApp.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace WpfAppTest01
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            NLogService nLogService = new NLogService(Directory.GetCurrentDirectory());
+            var host = nLogService.BuildHost(CreateHostBuilder);
+            var mainWindow = host.Services.GetRequiredService<MainWindow>();
+            mainWindow.Show();
+        }
+
+        private IConfiguration Configuration { get; set; }
+        public IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration(x=>Configuration = x.Build())
+                .ConfigureServices(ConfigureServices);
+
+        private void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<EFDatabaseContext>(option =>
+                {
+                    option.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+                });
+            services.AddScoped(typeof(MainWindow));
+        }
+    }
+}
---
+++ 16 - Working with Relationships 2/DataApp/WpfAppTest01/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using DataApp.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfAppTest01
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ILogger<MainWindow> _logger;
+        private EFDatabaseContext _dbContext;
+
+        public MainWindow(ILogger<MainWindow> logger, EFDatabaseContext dbContext)
+        {
+            _logger = logger;
+            _dbContext = dbContext;
+            _logger.LogInformation("MainWindow");
+            InitializeComponent();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            Button button = e.Source as Button;
+            if (button == null) return;
+            if (button.Name == nameof(btnQueryData))
+            {
+                _logger.LogDebug(_dbContext.Products.Count().ToString());
+                var products = _dbContext.Products.Include(x => x.ProductShipments).ThenInclude(x => x.Shipment).ToList();
+                foreach (var product in products)
+                {
+                    _logger.LogDebug($"{product.Name}");
+                    foreach (var c in product.ProductShipments)
+                    {
+                        var shipment = c.Shipment;
+                        _logger.LogDebug($">>{shipment.ShipperName},{shipment.StartCity}-{shipment.EndCity}");
+                    }
+                }
+            }
+        }
+    }
+}
